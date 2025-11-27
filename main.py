ad3977bea8542a803c1610aa7ac530e9ba408fc6
Marcus Laguerre <110794840+iMarcraft@users.noreply.github.com>
2025-11-26 22:38:24 -0500

    time related variables added & clock in function complete

--- main.py
+++ main.py
@@ -1,58 +1,85 @@
 '''
-    Calculate Pays
-
-Use time clock data in csv to calculate estimate next payout
+    TimeAndPay
+    
+    Track hours on the clock with your own clocking machine.
+    Track expected net pay each pay period too!
 '''
-
-import pandas as pd
 import os
-import notifications as noti
-import datetime
-#from datetime import date
-import sys
+import platform
+import pandas as pd
+import datetime as dt
 
 if __name__ == '__main__':
-
-    neededHours = 520
-
-    device = "iPhone"
-    #print(sys.argv)    # debug iPhone
-
-    # file directories
+    
+    # if mobile, enable notifications
+    is_mobile = 'iOS' or 'iPadOS' or 'Android'  # mobile systems    
+    if platform.system() == is_mobile:
+        import notifications
+        
+        dir_delim = '/'
+        
+        # use notifications to display messages
+        def notify(message):
+            notifications.send_notification(notifications.Notification(message))
+    else:
+        dir_delim = '\\' if platform.system() == "Windows" else '/'
+        
+        def notify(message):
+            print(message)
+    
+    # file directory structure
+    parent_dir = os.path.abspath(__file__)  # full path to current file
+    parent_dir = os.path.dirname(parent_dir)   # parent directory of current file
+    
     data_dir = 'data'
-    #output_dir = 'output'
+    
+    employer_dir = 'skillstorm'
+    
+    file_name = 'timeclocks'
+    
+    file_extension = '.csv'
 
-    # file
-    file_name = 'timeclocks.csv'
+    file_full = file_name + file_extension
+    
+    # build path
+    path = os.path.join(parent_dir, data_dir, employer_dir, file_full)
 
-    # build file location
-    path = os.path.join(data_dir, file_name)
-    #print("Path: " + path)    # debug
+    directory_name = f"{data_dir}{dir_delim}{employer_dir}"
 
-    # read csv into data frame
-    orig = pd.read_csv(path)
-    orig['date'] = orig['date'].astype('string')
-    orig['weekday'] = orig['weekday'].astype('string')
+    # if path doesn't exists, create it    
+    if not os.path.exists(path):
+        try:
+            os.makedirs(f"{directory_name}", exist_ok=True)
+            # print(f"Directory '{directory_name}' created successfully.")
+        except PermissionError:
+            notify(f"Permission denied: Unable to create '{directory_name}'.")
+        except Exception as e:
+            notify(f"An error occured: {e}")
+        finally:
+            with open(f"{directory_name}{dir_delim}{file_full}", 'x', encoding='utf-8') as file:   # x mode safely creates new file, abort if already exists
+                file.write("date,weekday,clocked_in,lunch_in,lunch_out,clocked_out")
 
-    # clean up data
-    # data = orig.dropna()
-    data = orig
+    # section csv data using pandas
+    data = pd.read_csv(path)
 
-    # time variables relative to present moment
-    this_day = datetime.datetime.now()
-    today = str(this_day.month) + '/' + (str(this_day.day) if int(str(this_day.day)) >= 10 else '0' + str(this_day.day))
-    yesterday = str(this_day - datetime.timedelta(days=1))
-    yesterday = yesterday.split('-')
-    yesterday = yesterday[1] + '/' + yesterday[2][:2]
+    # time variables
+    date_delim = '.'
+    present = dt.datetime.now()
+    today = str(present.strftime('%y')) + date_delim + str(present.month) + date_delim + str(present.day)
 
-    this_hour = str(this_day.hour)
-    this_min = str(this_day.minute) if this_day.minute >= 10 else '0' + str(this_day.minute)
+    def daysAgo(numOfDays):
+        date_of_day = str(present - dt.timedelta(days=numOfDays)).split('-')
+        date_of_day = date_of_day[0][2:] + date_delim + date_of_day[1] + date_delim + date_of_day[2][:2]
 
-    # print('this_hour: ' + this_hour)    # debug
-    # print('this_min: ', this_day.minute)      # debug
+        return date_of_day
+
+    yesterday = daysAgo(1)
+
+    hour = str(present.hour)
+    minute = str(present.minute)
 
     # array of weekdays
-    weekdays = ["M", "T", "W", "TH", "F", "SAT", "SUN"]
+    weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
     # timeclock dictionary
     timeclock = {}
@@ -60,253 +87,91 @@ if __name__ == '__main__':
     curr_week = 0
 
     # create an array from each row
-    for rows in range(data.shape[0]):
-        df = data.iloc[rows]
-        if not pd.isna(df['clocked_in']) and not pd.isna(df['clocked_out']):
-            timeclock[df['date']] = str(int(df['clocked_in'])) + " - " + str(int(df['clocked_out']))
-        elif not pd.isna(df['clocked_in']) and pd.isna(df['clocked_out']):
-            if df['date'] == today:
-                timeclock[df['date']] = str(int(df['clocked_in'])) + " - " + this_hour + this_min
+    #
+    #
+    #
+    #
+
+    def clockIn(lastChar):
+        # clock in
+        with open(path, 'a') as file:
+
+            # if not clocked in (not on clock)
+            if last_char != file_delim:
+                # clock in
+                file.write(f"\"{today}\"{file_delim}\'{weekdays[present.weekday()]}\'{file_delim}{hour}{minute}{file_delim}")
+
+                # return confirmation message
+                message = "Software Developing Time!"
+
             else:
-                timeclock[df['date']] = str(int(df['clocked_in'])) + " - " + str(100)
-        elif pd.isna(df['clocked_in']) and not pd.isna(df['clocked_out']):
-            timeclock[df['date']] = str(1630) + " - " + str(int(df['clocked_out']))
+                # return reason of failure
+                message = "Unable to clock in."
+            notify(message)
+
+    def clockOut()
+        # if clocked in
+        if timeclock(today) and not pd.isna(data == today][clocked_in]):
+            # clock out
+            with open(path, 'a') as file:
+                file.write(f"{hour}{minute}\n")
+            # return confirmation message
+            message = "Nice work! Let's develop some more another time."
         else:
-            timeclock[df['date']] = str(1630) + " - " + str(100)
-            # print(df['date'], ":", timeclock[df['date']])     # debug
+            # return reason of failure clock out
+            message = "Are you even allowed to clock out right now?"
+        notify(message)
 
-        # build week associations
-        new_week = len(weeks) == 0 or df['weekday'] == 'M' or df['weekday'] in weeks[curr_week]
 
-        if new_week:
-            if rows != 0:
-                # increment week
-                curr_week += 1
 
-            weeks.append(curr_week)
+    # prompt
+    action = input("Clock in(ci), Lunch in(li), Lunch out(lo), Clock out(co), Info(i), Pay(p): ")
+    file_delim = ','
 
-            # initialize dictionary for new week
-            weeks[curr_week] = {}
+    if action == 'ci':
 
-        # add current date to dictionary
-        weeks[curr_week][df['weekday']] = timeclock[df['date']]
+        with open(path, 'r+') as file:
 
-    def printOrNotify(message):
-        print(message) if device == "computer" else noti.send_notification(noti.Notification(message))
+            file_str = file.read()
+            last_char = file_str[-1]
 
-    def clockIn(file_path, day_of_week, curr_hour, curr_min, message):
-        with open(file_path, 'a') as file:
-            file.write('\"' + today + '\"'
-                       + ',\"' + weekdays[datetime.datetime.today().weekday()] + '\"'
-                       + ',' + this_hour + this_min)
+            clockIn(last_char)
 
-        printOrNotify(message)
+            
+    elif action == 'li':
+        # if on clock
+            # punch out for lunch
+            # return confirmation message
+        # else
+            # return reason of failure
+        notify("Lunch In")
+    elif action == 'lo':
+        # if off clock
+            # punch in from lunch
+            # return confirmation message
+        # else
+            # return reason of failure
+        notify("Lunch Out")
+    elif action == 'co':
+       clockOut() 
 
-    def clockOut(file_path, curr_hour, curr_min, message):
-        with open(file_path, 'a') as file:
-            file.write(',' + curr_hour + curr_min
-                       + '\n')
+        notify("Clock Out")
+    elif action == 'i':
+        info_type = input("Generic(g) or Search(s): ")
 
-            printOrNotify(message)
-
-    '''
-    Calculate the amount of hours worked from a formated string:
-    
-    "hh:mm - hh:mm"
-    
-    Round to the nearest ones place. Return the amount of hours
-    rounded.
-    '''
-    def calcDayHours(shift):
-        # grab clock in time
-        clocked_times = shift.split(' - ')
-        clock_in = clocked_times[0]
-        clock_out = clocked_times[1]
-
-        # 00:00 check
-        if int(clock_in) < 10:
-            clock_in = '00' + str(clock_in)
-        elif int(clock_in) < 100:
-            clock_in = '0' + str(clock_in)
-
-        if int(clock_out) < 10:
-            clock_out = '00' + str(clock_out)
-        elif int(clock_out) < 100:
-            clock_out = '0' + str(clock_out)
-
-        #print(clock_in, clock_out)    # debug
-
-        # minutes til equal
-        minute_diff = 0
-        curr = int(clock_in[-2:])
-        while curr != int(clock_out[-2:]):
-            curr += 1
-
-            if curr == 60:   # minute max
-                curr = 0
-                minute_diff += 1
-                clock_in = str(int(clock_in) + minute_diff + 40) if int(clock_in) >= 1000 and int(clock_in) <= 2300 else str(int(0))
-            else:
-                minute_diff += 1
-
-        # hours til equal
-        hour_diff = 0
-        curr = int(clock_in[:2]) if int(clock_in) >= 1000 else int(clock_in[0])
-        while curr != (int(clock_out[:2]) if int(clock_out) >= 1000 else int(clock_out[0])):
-            curr += 1
-
-            if curr == 24:   # hour max
-                curr = 0
-                hour_diff += 1
-            else:
-                hour_diff += 1
-
-        # remove lunch break if worked more than 6 hours
-        if hour_diff >= 6:
-            if minute_diff < 30:
-                hour_diff -= 1
-                minute_diff = 60 - (30 - minute_diff)
-            else:
-                minute_diff -= 30
-
-        # print("This day was a", hour_diff, "hour(s) and", minute_diff, "minute shift.")   # debug
-        return hour_diff + round((minute_diff / 60), 2)
-
-    def calcWeekHours(week):
-        monday = calcDayHours(week['M']) if 'M' in week else 0
-        tuesday = calcDayHours(week["T"]) if 'T' in week else 0
-        wednesday = calcDayHours(week["W"]) if 'W' in week else 0
-        thursday = calcDayHours(week["TH"]) if 'TH' in week else 0
-        friday = calcDayHours(week["F"]) if 'F' in week else 0
-
-        return round(monday + tuesday + wednesday + thursday + friday, 2)
-
-    def calcTotalHours(timeclock):
-        sum = 0
-        for hours in timeclock.values():
-            sum += calcDayHours(hours)
-
-        return round(sum, 2)
-
-    def calcWeekPay(week, hourly):
-        hours = calcWeekHours(week)
-
-        if hours > 40:
-            gross = hourly * 40
-            overtimeHourly = hourly + (hourly/2)
-            overtimeHours = hours - 40
-            overtimePay = overtimeHourly * overtimeHours
-            gross = gross + overtimePay
+        if info_type == 'g':
+            # do something
+            notify("Gathering generic info...")
+        elif info_type == 's':
+            # do something
+            notify("Calculating searched info...")
         else:
-            gross = hourly * hours
-
-        soc_sec_ee = 00.062 * gross
-        med_ee = 0.014 * gross
-        federal_wh = 0.076 * gross
-        vt_wh = 0.03 * gross
-        vt_cc_ee = 0.001 * gross
-
-        net = gross - (soc_sec_ee + med_ee + federal_wh + vt_wh + vt_cc_ee)
-
-        all_earnings = [round(hours,2), round(net,2), round(gross,2)]
-
-        return all_earnings
-    
-    hours_total = calcTotalHours(timeclock)
-
-    user_input = input("Clock In(in), Clock Out(out), Info(info), Search(search) Pay(pay)") if device == "computer" else (sys.argv[1] if len(sys.argv) > 1 else 'info')
-    # user_input = "search"   # debug
-    
-    if user_input == 'in':
-        message = "Welcome to work, partner 😏"
-
-        if today not in timeclock:  # if today not in timeclock
-
-            # check for yesterday
-            try:
-                hasYesterday = pd.isna(orig[orig['date'] == yesterday]['clocked_in']).item
-                if hasYesterday:
-                    if not pd.isna(orig[orig['date'] == yesterday]['clocked_out']).item():
-                        clockIn(path, weekdays[this_day.weekday()], this_hour, this_min, message)
-                    else:
-                        message = "You haven't clocked out from yesterday's shift yet"
-                        printOrNotify(message)
-            except ValueError:  # no clock in for yesterday
-                clockIn(path, weekdays[this_day.weekday()], this_hour, this_min, message)
-
-        else:   # today IS in timeclock
-            message = "You've already clocked in once today"
-            printOrNotify(message)
-    elif user_input == 'out':
-        message = "Good work today, get some rest"
-
-        # print("Today is: " + today + "\nYesterday was: " + yesterday)    # debug
-
-        if yesterday in timeclock and pd.isna(orig[orig['date'] == yesterday]['clocked_out']).item():
-            clockOut(path, this_hour, this_min, message)
-        elif today in timeclock:
-            df = orig[orig['date'] == today]
-            if pd.isna(df['clocked_out']).item():
-                clockOut(path, this_hour, this_min, message)
-            else:
-                message = "You've already clocked out for today's shift"
-                printOrNotify(message)
-        else:
-            message = "You haven't clocked in yet today"
-            printOrNotify(message)
-    elif user_input == 'info':
-        # return hours of today's shift
-        hours_today = calcDayHours(timeclock[today]) if today in timeclock else 0
-
-        # return hours of latest week
-        hours_this_week = calcWeekHours(weeks[len(weeks) - 1])
-        
-        delim = '\t'
-
-        message = "Today: " + str(hours_today) + delim + "Week: " + str(hours_this_week) + delim + "Total: " + str(hours_total)
-        printOrNotify(message)
-    elif user_input == 'search':
-        search_for = input("date, week, or total: ") if device == "computer" else sys.argv[2]
-        # search_for = "week"   # debug
-        if search_for == 'date':
-            day = input("Enter date (mm/dd): ").split('/') if device == "computer" else sys.argv[3].split('/')
-            searching = day[0] + '/' + (day[1] if int(day[1]) >= 10 else '0' + str(int(day[1])))
-
-            try:
-                hours = calcDayHours(timeclock[searching])
-                message = "This was a " + str(hours) + " hour shift"
-            except KeyError:
-                hours = 0
-                message = "Shift not found"
-            finally:
-                printOrNotify(message)
-
-        elif search_for == 'week':
-            searching = int(input("Week # (1-" + str(len(weeks)) + "): ")) if device == "computer" else int(sys.argv[3])
-            # print(searching)    # debug
-
-            if searching > 0:
-                try:
-                    hours = calcWeekHours(weeks[searching - 1])     # match week to array index
-                    message = "\nYou worked " + str(hours) + " hours for the selected week"
-                except IndexError:
-                    hours = 0
-                    message = "Invalid week number. Please try again."
-            else:
-                message = "You're not serious.."
-
-            printOrNotify(message)
-
-        elif search_for == 'total':
-            hours = calcTotalHours(timeclock)
-            remHours = round(neededHours - hours, 2)
-            remDays = round(remHours / 8, 2)
-
-            message = str(hours) + "/" + str(neededHours) + "\nHours Rem: " + str(remHours) + "\nDays Rem: " + str(remDays)
-            printOrNotify(message)
-    elif user_input == 'pay':
-        week = int(input("Week #: ")) if device == "computer" else int(sys.argv[2])
-        hourly = float(input("Hourly pay: ")) if device == "computer" else float(sys.argv[3])
-
-        message = "Hours: " + str(calcWeekPay(weeks[week-1], hourly)[0]) + "\tNet: " + str(calcWeekPay(weeks[week-1], hourly)[1]) + "\tGross: " + str(calcWeekPay(weeks[week-1], hourly)[2])
-        printOrNotify(message)
+            # error handling
+            notify("Somthing went wrong... :(")
+        # do sumn
+    elif action == 'p':
+        # do things
+        notify("Pay")
+    else:
+        # error handling
+        notify("Yup, something definitely went wrong.. :(")
