21f4b5aeb78c492212591efa768946486799a9e5
Marcus Laguerre <110794840+iMarcraft@users.noreply.github.com>
2025-11-30 00:55:52 -0500

    reformatted orders & functions

--- main.py
+++ main.py
@@ -35,9 +35,11 @@ if __name__ == '__main__':
     
     employer_dir = 'skillstorm'
     
-    file_name = 'timeclocks'
+    file_name = 'timeclocks 2'
     
     file_extension = '.csv'
+    
+    file_delim = ','
 
     file_full = file_name + file_extension
     
@@ -57,7 +59,7 @@ if __name__ == '__main__':
             notify(f"An error occured: {e}")
         finally:
             with open(f"{directory_name}{dir_delim}{file_full}", 'x', encoding='utf-8') as file:   # x mode safely creates new file, abort if already exists
-                file.write("date,weekday,clocked_in,lunch_in,lunch_out,clocked_out")
+                file.write("date,weekday,clocked_in,lunch_in,lunch_out,clocked_out\n")
 
     # section csv data using pandas
     data = pd.read_csv(path)
@@ -76,7 +78,7 @@ if __name__ == '__main__':
     yesterday = daysAgo(1)
 
     hour = str(present.hour)
-    minute = str(present.minute)
+    minute = str(present.minute) if present.minute >= 10 else '0' + str(present.minute)
 
     # array of weekdays
     weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
@@ -85,93 +87,137 @@ if __name__ == '__main__':
     timeclock = {}
     weeks = []
     curr_week = 0
-
-    # create an array from each row
-    #
-    #
-    #
-    #
-
-    def clockIn(lastChar):
-        # clock in
+        
+    # define clock in
+    def clockIn():
+        
+        # open file in append mode
         with open(path, 'a') as file:
-
-            # if not clocked in (not on clock)
-            if last_char != file_delim:
-                # clock in
-                file.write(f"\"{today}\"{file_delim}\'{weekdays[present.weekday()]}\'{file_delim}{hour}{minute}{file_delim}")
-
-                # return confirmation message
-                message = "Software Developing Time!"
-
-            else:
-                # return reason of failure
-                message = "Unable to clock in."
-            notify(message)
-
-    def clockOut()
-        # if clocked in
-        if timeclock(today) and not pd.isna(data == today][clocked_in]):
-            # clock out
-            with open(path, 'a') as file:
-                file.write(f"{hour}{minute}\n")
-            # return confirmation message
-            message = "Nice work! Let's develop some more another time."
-        else:
-            # return reason of failure clock out
-            message = "Are you even allowed to clock out right now?"
-        notify(message)
-
-
-
+            
+            # clock in
+            file.write(f"\"{today}\"{file_delim}\'{weekdays[present.weekday()]}\'{file_delim}{hour}{minute}")
+    
+    # define clock out
+    def clockPunch():
+        
+        # open file in append mode
+        with open(path, 'a') as file:
+            # write current hour & minute
+            file.write(f"{hour}{minute}")
+        
+    def printDelim():
+        
+        # open file in append mode
+        with open(path, 'a') as file:
+            file.write(file_delim)  # write delimiter character
+        
     # prompt
     action = input("Clock in(ci), Lunch in(li), Lunch out(lo), Clock out(co), Info(i), Pay(p): ")
-    file_delim = ','
 
-    if action == 'ci':
+    if action == "ci" or action == "li" or action == "lo" or action == "co" or action == "i" or action == "p":
 
-        with open(path, 'r+') as file:
+        # empty data value
+        null_val = 'null'
 
-            file_str = file.read()
-            last_char = file_str[-1]
+        # load timeclock data into array
+        for rows in range(data.shape[0]):
 
-            clockIn(last_char)
+            df = data.iloc[rows]    # store current row in df
 
+            times = [df['clocked_in'], df['lunch_in'], df['lunch_out'], df['clocked_out']]
+            timeclock['date'] = [null_val, null_val, null_val, null_val]
+
+            for  i in range (data.shape[1] - 2):
+                timeclock['date'][i] = str(times[i]) if not pd.isna(times[i]) else null_val
+                print(timeclock['date'][i])
+
+
+        def checkPunch(punch):
+            if data.iloc[0][punch] != null_val:
+                return True
+            else:
+                return False
+
+
+        if action == 'ci':      # clock in
+
+            # if clocked in == null
+            if not checkPunch('clocked_in'):
+                clockIn()
+                printDelim()
+                message = "[Welcome message]"
+            else:
+                message = "[Clock In failed message]"
+
+            notify(message)
+
+        elif action == 'li':    # begin lunch
+            if checkPunch('clocked_in') and not checkPunch('lunch_in'):
+                clockPunch()
+                printDelim()
+                message = "Hungry?"
+            elif not checkPunch('clocked_in'):
+                message = "You're not even on the clock..."
+            elif checkPunch('lunch_in'):
+                message = "Eating began at " + str(data.iloc[0]['lunch_in']) + ", remember?"
+            else:
+                message = "[Error slipping out to lunch]"
+
+            notify(message)
+
+        elif action == 'lo':    # end lunch
+            if checkPunch('clocked_in') and checkPunch('lunch_in') and not checkPunch('lunch_out'):
+                clockPunch()
+                printDelim()
+                message = "The developing continues :)"
+            elif not checkPunch('clocked_in'):
+                message = "You're not even on the clock..."
+            elif not checkPunch('lunch_in'):
+                message = "You must begin your lunch to end your lunch."
+            elif checkPunch('lunch_out'):
+                message = "Lunch can't end twice. Focus up on the project."
+            else:
+                message = "[Error lunch not ended]"
+
+            notify(message)
             
-    elif action == 'li':
-        # if on clock
-            # punch out for lunch
-            # return confirmation message
-        # else
-            # return reason of failure
-        notify("Lunch In")
-    elif action == 'lo':
-        # if off clock
-            # punch in from lunch
-            # return confirmation message
-        # else
-            # return reason of failure
-        notify("Lunch Out")
-    elif action == 'co':
-       clockOut() 
+        elif action == 'co':    # clock out
+            if checkPunch('clocked_in') and checkPunch('lunch_in') and checkPunch('lunch_out') and not checkPunch('clocked_out'):
+                clockPunch()
+                message = "Nice work. Let's develop some more tomorrow!"
+            elif not checkPunch('clocked_in'):
+                message = "You're not even on the clock..."
+            elif not checkPunch('lunch_in'):
+                message = "You must being your lunch to end your lunch."
+            elif not checkPunch('lunch_out'):
+                message = "End your lunch first"
+            elif checkPunch('clocked_out'):
+                message = "You're already off the clock. Seems you're tired, rest up."
+            else:
+                message = "Aw, great. You broke something."
 
-        notify("Clock Out")
-    elif action == 'i':
-        info_type = input("Generic(g) or Search(s): ")
+            notify(message)
 
-        if info_type == 'g':
-            # do something
-            notify("Gathering generic info...")
-        elif info_type == 's':
-            # do something
-            notify("Calculating searched info...")
-        else:
-            # error handling
-            notify("Somthing went wrong... :(")
-        # do sumn
-    elif action == 'p':
-        # do things
-        notify("Pay")
+        elif action == 'i':     # info
+            info_type = input("Generic(g) or Search(s): ")
+
+            if info_type == 'g':
+                # do something
+                notify("Gathering generic info...")
+            elif info_type == 's':
+                # do something
+                notify("Calculating searched info...")
+            else:
+                # error handling
+                notify("Somthing went wrong... :(")
+            # do sumn
+        elif action == 'p':
+            # do things
+            notify("Pay")
+
     else:
-        # error handling
-        notify("Yup, something definitely went wrong.. :(")
+        # handle error message
+        notify("Yup, you messed something up buddy.")
+
+    
+        
